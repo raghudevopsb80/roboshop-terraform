c8521234acca04daa608b5a04ab67efb8018d484
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-26 06:27:34 +0530

    VPC Module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -94,3 +94,30 @@ resource "aws_route_table" "db" {
     Name = "db-rt-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
+
+## Route table association
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.*.id[count.index]
+}
+
+resource "aws_route_table_association" "web" {
+  count          = length(var.web_subnets)
+  subnet_id      = aws_subnet.web.*.id[count.index]
+  route_table_id = aws_route_table.web.*.id[count.index]
+}
+
+resource "aws_route_table_association" "app" {
+  count          = length(var.app_subnets)
+  subnet_id      = aws_subnet.app.*.id[count.index]
+  route_table_id = aws_route_table.app.*.id[count.index]
+}
+
+resource "aws_route_table_association" "db" {
+  count          = length(var.db_subnets)
+  subnet_id      = aws_subnet.db.*.id[count.index]
+  route_table_id = aws_route_table.db.*.id[count.index]
+}
+
+
