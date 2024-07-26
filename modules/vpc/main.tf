37c9a483b35026a49075a8a7afccb73a38f49fae
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-26 06:30:43 +0530

    VPC Module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -59,6 +59,10 @@ resource "aws_route_table" "public" {
   count  = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
   tags = {
     Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
@@ -121,3 +125,11 @@ resource "aws_route_table_association" "db" {
 }
 
 
+## Internet gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
