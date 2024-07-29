f22ec81cadb1a24905890bc868065be66a5be0d2
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-29 07:19:22 +0530

    Ec2 Module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -97,3 +97,43 @@ resource "aws_route53_record" "instance" {
   records = [aws_instance.main.*.private_ip[count.index]]
 }
 
+
+resource "aws_security_group" "load-balancer" {
+  count       = var.asg ? 1 : 0
+  name        = "${var.name}-${var.env}-alb-sg"
+  description = "${var.name}-${var.env}-alb-sg"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = var.allow_sg_cidr
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-alb-sg"
+  }
+}
+
+resource "aws_lb" "main" {
+  count              = var.asg ? 1 : 0
+  name               = "${var.name}.${var.env}"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load-balancer.*.id[count.index]]
+  subnets            = var.subnet_ids
+
+  tags = {
+    Environment = "${var.name}.${var.env}"
+  }
+}
+
