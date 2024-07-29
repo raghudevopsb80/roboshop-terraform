4bc916c92fef36d1996ddd3e1484d6ae6bab7168
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-29 06:40:03 +0530

    Ec2 Module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -94,6 +94,6 @@ resource "aws_route53_record" "instance" {
   name    = "${var.name}.${var.env}"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.main.*.id[count.index]]
+  records = [aws_instance.main.*.private_ip[count.index]]
 }
 
