c84b55edab27b50d543ad5258c58cec9ac5337f8
Raghu K <raghuk.vit@gmail.com>
2024-08-01 07:20:16 +0530

    ALB Module

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -104,7 +104,7 @@ resource "aws_lb_listener_rule" "listener-rule" {
 
   condition {
     host_header {
-      values = [aws_route53_record.lb.name]
+      values = [aws_route53_record.lb.fqdn]
     }
   }
 }
