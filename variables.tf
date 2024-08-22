f380e9dbf8a8ad26fcc991856566c2f065afa0d3
Raghu K <raghuk.vit@gmail.com>
2024-08-22 06:45:40 +0530

    Learn Kubernetes

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "vpc" {}
 variable "env" {}
-variable "apps" {}
 variable "bastion_nodes" {}
 variable "db" {}
 variable "vault_token" {}
 variable "zone_id" {}
-variable "load_balancers" {}
 
