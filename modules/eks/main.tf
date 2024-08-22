81c58bb89b6ada94b6256a61ab32149d121bfb71
Raghu K <raghuk.vit@gmail.com>
2024-08-22 07:24:25 +0530

    Learn Kubernetes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,6 +6,10 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 }
 
 resource "aws_eks_node_group" "main" {
