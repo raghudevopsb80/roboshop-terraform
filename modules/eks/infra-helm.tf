599c3bd09f7ad0da0b2303d4171c09964478f79a
Raghu K <raghuk.vit@gmail.com>
2024-08-28 07:26:53 +0530

    RoboShop Kubernetes

--- modules/eks/infra-helm.tf
+++ modules/eks/infra-helm.tf
@@ -4,6 +4,7 @@ resource "null_resource" "kube-config" {
   provisioner "local-exec" {
     command =<<EOF
 aws eks update-kubeconfig --name ${var.env}-eks
+kubectl apply -f /opt/vault-token.yaml
 EOF
   }
 }
@@ -17,7 +18,32 @@ resource "helm_release" "external-secrets" {
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
   namespace  = "kube-system"
+  wait       = true
 }
 
+resource "null_resource" "external-secrets-store" {
+  depends_on = [helm_release.external-secrets]
 
+  provisioner "local-exec" {
+    command =<<EOF
+kubectl apply -f - <<EOK
+apiVersion: external-secrets.io/v1beta1
+kind: ClusterSecretStore
+metadata:
+  name: vault-backend
+spec:
+  provider:
+    vault:
+      server: "http://vault-internal.rdevopsb80.online8200/"
+      path: "roboshop-${var.env}"
+      version: "v2"
+      auth:
+        tokenSecretRef:
+          name: "vault-token"
+          key: "token"
+          namespace: kube-system
+EOK
+EOF
+  }
+}
 
